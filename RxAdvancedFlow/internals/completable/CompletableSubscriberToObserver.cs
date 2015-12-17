ebe4f6c064a2cd635ec528bc747e574b3087f8ba
Dávid Karnok <akarnokd@gmail.com>
2015-12-17 23:51:11 +0100

    VS tracking bug again...

---
+++ RxAdvancedFlow/internals/completable/CompletableSubscriberToObserver.cs
@@ -0,0 +1,49 @@
+﻿using ReactiveStreamsCS;
+using RxAdvancedFlow.internals.disposables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RxAdvancedFlow.internals.completable
+{
+    sealed class CompletableSubscriberToObserver<T> : IObserver<T>, IDisposable
+    {
+        readonly ICompletableSubscriber cs;
+
+        IDisposable d;
+
+        public CompletableSubscriberToObserver(ICompletableSubscriber s)
+        {
+            cs = s;
+        }
+
+        public void Dispose()
+        {
+            DisposableHelper.Terminate(ref d);
+        }
+
+        public void Set(IDisposable value)
+        {
+            DisposableHelper.Set(ref d, value);
+        }
+
+        public void OnCompleted()
+        {
+            cs.OnComplete();
+        }
+
+        public void OnError(Exception error)
+        {
+            cs.OnError(error);
+        }
+
+        public void OnNext(T value)
+        {
+            // ignored
+        }
+
+        
+    }
+}
